65f7f0d5f942d76d110938e2a2adf791ac287f13
jonathanroll <75393433+jonathanroll@users.noreply.github.com>
2023-09-16 17:56:43 +0000

    vsc debug setup

--- jsonstream.mjs
+++ jsonstream.mjs
@@ -8,6 +8,12 @@ import { MongoClient } from 'mongodb';
 async function insertDoc(client, doc){
     let result = await client.db('survey').collection('products').insertOne(doc);
     console.log(`New listing created with the following id: ${result.insertedId}`);
+    return result;
+}
+async function insertDocs(client, docs){
+    return await client.db('survey').collection('products').insertMany(docs);
+    // console.log(`New listing created with the following id: ${result.insertedId}`);
+    // return result;
 }
 
 function main(streamData) {
@@ -15,10 +21,17 @@ function main(streamData) {
     let uri = 'mongodb://black.local';
     let client = new MongoClient(uri);
 
-    streamData.on('data',(data)=>{
-        JSON.parse(data).products.forEach(async (e)=>{
-            await insertDoc(client,e);
-        })
+    // streamData.on('data',(data)=>{
+    //     JSON.parse(data).products.forEach(async (e)=>{
+    //         await insertDoc(client,e);
+    //     })
+    // })
+    streamData.on('data',async (data)=>{
+        console.error('err streamdata',data);
+        console.log('log streamdata',data);
+        // return await insertDocs(client, JSON.parse(data).products);
+        return await insertDocs(client, JSON.parse(data).products)
+            .then(p=>p);
     })
     .on('end',()=>{console.log('end of stream')});
 
